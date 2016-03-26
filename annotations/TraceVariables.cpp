ee80c579445e61b7588b191831e181a9f3bae7d9
Sol Boucher <sboucher@cmu.edu>
2016-03-26 11:05:12 -0400

    annotations: Start TraceVariables pass for associating values with high-level variables
    This is accomplished using debugging metadata deposited by the frontend. It's
    currently only implemented for locals.

---
+++ annotations/TraceVariables.cpp
@@ -0,0 +1,59 @@
+#include "TraceVariables.h"
+
+#include <llvm/IR/Function.h>
+#include <llvm/IR/IntrinsicInst.h>
+#include <llvm/Support/raw_ostream.h>
+#include <cassert>
+
+using llvm::BasicBlock;
+using llvm::Constant;
+using llvm::DbgDeclareInst;
+using llvm::DbgInfoIntrinsic;
+using llvm::DbgValueInst;
+using llvm::DILocalVariable;
+using llvm::dyn_cast;
+using llvm::Function;
+using llvm::Instruction;
+using llvm::isa;
+using llvm::RegisterPass;
+using llvm::Value;
+using std::unordered_map;
+
+char TraceVariables::ID = 0;
+
+TraceVariables::TraceVariables() :
+	FunctionPass(ID) {}
+
+bool TraceVariables::runOnFunction(Function &fun) {
+	for(BasicBlock &block : fun.getBasicBlockList())
+		for(Instruction &inst : block.getInstList())
+			if(DbgInfoIntrinsic *annot = dyn_cast<DbgInfoIntrinsic>(&inst)) {
+				Value *key = valOf(*annot);
+				assert(key);
+				if(isa<Constant>(key))
+					continue;
+
+				assert(!locals.count(key));
+				locals.emplace(key, *annot);
+			}
+
+	return false;
+}
+
+Value *TraceVariables::valOf(DbgInfoIntrinsic &annot) {
+	if(DbgDeclareInst *decl = dyn_cast<DbgDeclareInst>(&annot))
+		return decl->getAddress();
+	if(DbgValueInst *val = dyn_cast<DbgValueInst>(&annot))
+		return val->getValue();
+	return NULL;
+}
+
+DILocalVariable *TraceVariables::varOf(DbgInfoIntrinsic &annot) {
+	if(DbgDeclareInst *decl = dyn_cast<DbgDeclareInst>(&annot))
+		return decl->getVariable();
+	if(DbgValueInst *val = dyn_cast<DbgValueInst>(&annot))
+		return val->getVariable();
+	return NULL;
+}
+
+static RegisterPass<TraceVariables> X("tracevars", "Trace Variables", true, true);
---
+++ annotations/TraceVariables.h
@@ -0,0 +1,30 @@
+#ifndef TRACE_VARIABLES_H_
+#define TRACE_VARIABLES_H_
+
+#include <llvm/Pass.h>
+#include <unordered_map>
+
+namespace llvm {
+class DbgInfoIntrinsic;
+class DILocalVariable;
+class Value;
+}
+
+class TraceVariables : public llvm::FunctionPass {
+public:
+	static char ID;
+
+private:
+	std::unordered_map<llvm::Value *, llvm::DbgInfoIntrinsic &> locals;
+
+public:
+	TraceVariables();
+
+	bool runOnFunction(llvm::Function &) override;
+
+private:
+	static llvm::Value *valOf(llvm::DbgInfoIntrinsic &);
+	static llvm::DILocalVariable *varOf(llvm::DbgInfoIntrinsic &);
+};
+
+#endif
