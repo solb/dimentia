8407d341e4a2eb13932b96811a27a293da4a28ef
Goran Zuzic <gzuzic@cs.cmu.edu>
2016-04-23 00:48:12 -0400

    examples

--- tests/array-easy.c
+++ tests/array-easy.c
@@ -1,6 +1,5 @@
-int A[10];
-
-int main(void) {
+int main(void) {  
+  int A[10] = {1, 2, 3};
   A[0] += A[1]*A[1];
-  return 0;
+  return A[0];
 }
--- tests/temporary.c
+++ tests/temporary.c
@@ -1,5 +1,7 @@
+int a;
+
 int main(void) {
-  int a = 1;
+  a = 1;
   int b = 2;
   int c = 3;
   int x = a + b * c;
